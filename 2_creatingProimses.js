a78b76f879f9b6a682289b292561b03edb399781
elsonlim <elsonlim7@gmail.com>
2019-09-12 11:34:10 +0800

    fix logic, error thrown will cause failure to run when there is a failure function and not catch

--- 2_creatingProimses.js
+++ 2_creatingProimses.js
@@ -2,33 +2,57 @@ const success = () => console.log("success");
 const fail = () => console.log("fail");
 
 // When resolve, the first callback, success will be called
-new Promise((resolve, reject) => {
-  resolve();
-}).then(success, fail);
+const demo1 = () =>
+  new Promise((resolve, reject) => {
+    resolve();
+  }).then(success, fail);
 
 // When reject, the second callback, fail will be called
-new Promise((resolve, reject) => {
-  reject();
-}).then(success, fail); // fail
+const demo2 = () =>
+  new Promise((resolve, reject) => {
+    reject();
+  }).then(success, fail); // fail
 
 // We can chain a catch function to catch any error
-new Promise((resolve, reject) => {
-  throw new Error();
-  resolve();
-}).catch(e => console.log("error")); // error
+const demo3 = () =>
+  new Promise((resolve, reject) => {
+    throw new Error();
+    resolve();
+  }).catch(e => console.log("error")); // error
 
-// if there is a possibility of an error being thrown
-// you can handle the error in the catch
-new Promise((resolve, reject) => {
-  throw new Error();
-  resolve();
-})
-  .then(success, fail)
-  .catch(e => console.log("error here"))
-  .finally(() => console.log("happens no matter what")); // fail and not error
+// if an error is thrown, the fail method will run.
+const demo4 = () =>
+  new Promise((resolve, reject) => {
+    throw new Error();
+    resolve();
+  })
+    .then(success, fail)
+    .catch(e => console.log("not printed although there is error"))
+    .finally(() => console.log("happens no matter what")); // fail and not error
+
+// but if an error is thrown, and a fail method does not exist, catch will run
+const demo5 = () =>
+  new Promise((resolve, reject) => {
+    throw new Error();
+    resolve();
+  })
+    .then(success)
+    .catch(e => console.log("printed when there is no failure function"))
+    .finally(() => console.log("happens no matter what")); // fail and not error
+
+const demo6 = () =>
+  new Promise((resolve, reject) => {
+    throw new Error();
+    resolve();
+  })
+    .then(success, () => {
+      throw new Error();
+    })
+    .catch(e => console.log("printed when failure function throws an error"))
+    .finally(() => console.log("happens no matter what")); // fail and not error
 
 // is possible to create a promise that resolves immediately.
-Promise.resolve().then(success, fail); // success
+const demo7 = () => Promise.resolve().then(success, fail); // success
 
 // or fails immediately
-Promise.reject().then(success, fail); // success
+const demo8 = () => Promise.reject().then(success, fail); // success
